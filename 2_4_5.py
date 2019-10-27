7ccad62afc91dd09b1247cb96c24f757ef5ba148
Max <tataby93@gmail.com>
2019-10-27 12:11:19 +0300

    pyth code for exe 2_4_5

---
+++ 2_4_5.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import time 
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/cats"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_id("button")
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(7)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
